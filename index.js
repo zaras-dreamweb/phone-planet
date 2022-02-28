8483d82387964b8c512601b9d531fe9ccf628bfe
sadias-dreamweb <sadiatuzjohora11@gmail.com>
2022-03-01 02:12:24 +0600

    details button onclick function completed

--- index.js
+++ index.js
@@ -18,9 +18,8 @@ const loadPhones = () => {
 }
 
 const displayPhones = phones => {
-    // console.log(phones);
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
 
         const div = document.createElement('div');
         div.classList.add('col-lg-4');
@@ -30,7 +29,7 @@ const displayPhones = phones => {
             <div class="card-body text-center">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text fw-bold"> ${phone.brand}</p>
-                <button onclick="loadPhoneDetails("${phone.slug}")" type="button" class="btn btn-dark text-dark" style="background-color: rgb(202, 166, 231);">More Details</button>
+                <button onclick="loadPhoneDetails('${phone.slug}')" type="button" class="btn btn-dark text-dark" style="background-color: rgb(202, 166, 231);">More Details</button>
             </div>
         </div>
         `;
@@ -41,7 +40,31 @@ const displayPhones = phones => {
 
 const loadPhoneDetails = phoneId => {
     console.log(phoneId);
-    // fetch(`https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-${phoneId}`)
-    //     .then(res => res.json())
-    //     .then(data => console.log(data))
+    fetch(`https://openapi.programming-hero.com/api/phone/${phoneId}`)
+        .then(res => res.json())
+        .then(data => displayPhoneDetails(data.data))
+}
+
+const displayPhoneDetails = phone => {
+    console.log(phone);
+    const singlePhone = document.getElementById('single-phone');
+    const div = document.createElement('div');
+    div.innerHTML = `
+<div class="card mb-5" style="max-width: 500px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="${phone.image}" class="img-fluid rounded-start w-200" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${phone.name}</h5>
+        <p class="card-text">${phone.brand}</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+    `;
+
+    singlePhone.appendChild(div);
 }
